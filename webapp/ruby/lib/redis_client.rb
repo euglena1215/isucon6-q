1821f220ebeffb5a836146399a2c7cde02acd403
Shintani Teppei <teppest1215@gmail.com>
2019-04-30 12:03:07 +0900

    use host & port

--- webapp/ruby/lib/redis_client.rb
+++ webapp/ruby/lib/redis_client.rb
@@ -3,6 +3,6 @@ require 'redis/connection/hiredis'
 
 class RedisClient
   class << self
-    @redis = (Thread.current[:isu_redis] ||= Redis.new(path: '/tmp/redis.sock'))
+    @redis = (Thread.current[:isu_redis] ||= Redis.new(host: (ENV["REDIS_HOST"] || "127.0.0.1"), port: 6379))
   end
 end
